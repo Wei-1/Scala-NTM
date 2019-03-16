7bf2ec7ebff0db839bba7afd51867b5b5dcbd792
Wei Chen <weichentaiwan@gmail.com>
2019-03-16 13:42:31 +0800

    copytask-data-gen-init

--- copytask/src/main/scala/ntm/example/copytask.scala
+++ copytask/src/main/scala/ntm/example/copytask.scala
@@ -1,18 +1,18 @@
 package ntm.example
 
 object copytask {
-  def GenSeq(size: Int, vectorSize: Int): (Array[Array[float64]], Array[Array[Double]]) = {
+  def GenSeq(size: Int, vectorSize: Int): (Array[Array[Double]], Array[Array[Double]]) = {
     val data = new Array[Array[Double]](size)
     for(i <- 0 until data.size) {
-      data(i) = make([]float64, vectorSize)
-      for j := 0; j < len(data(i)); j++ {
-        data(i)(j) = float64(rand.Intn(2))
+      data(i) = new Array[Double](vectorSize)
+      for(j <- 0 until vectorSize) {
+        data(i)(j) = (Math.random * 2).toInt
       }
     }
 
     val input = new Array[Array[Double]](size * 2 + 2)
     for(i <- 0 until input.size) {
-      input(i) = make([]float64, vectorSize+2)
+      input(i) = new Array[Double](vectorSize + 2)
       if(i == 0) {
         input(i)(vectorSize) = 1
       } else if(i <= size) {
@@ -26,7 +26,7 @@ object copytask {
 
     val output = new Array[Array[Double]](size * 2 + 2)
     for(i <- 0 until output.size) {
-      output(i) = make([]float64, vectorSize)
+      output(i) = new Array[Double](vectorSize)
       if(i >= size + 2) {
         for(j <- 0 until vectorSize) {
           output(i)(j) = data(i - (size + 2))(j)
