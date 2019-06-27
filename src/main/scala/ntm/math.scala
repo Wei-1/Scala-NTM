132f851ad2c3af4cd1453f2c03b1d579ac0dffb6
weichen <weichen@apache.org>
2019-06-26 21:05:34 -0500

    mxnet-init-no-ci

--- src/main/scala/ntm/math.scala
+++ src/main/scala/ntm/math.scala
@@ -3,21 +3,19 @@ package com.scalaml.ntm
 import org.apache.mxnet._
 
 object math {
-  val machineEpsilon: Float = 2.2e-16
-  val machineEpsilonSqrt: Float = 1e-8 // Math.sqrt(machineEpsilon)
+  val machineEpsilon: Double = 2.2e-16
+  val machineEpsilonSqrt: Double = 1e-8 // Math.sqrt(machineEpsilon)
 
   // Sigmoid computes 1 / (1 + Math.exp(-x))
-  def Sigmoid(x: Float): Float =
-    1.0 / (1.0 + Math.exp(-x))
+  def Sigmoid(x: NDArray): NDArray =
+    NDArray.power(NDArray.exp(-x) + 1, -1)
 
-  def delta(a: Int, b: Int): Float =
-    if(a == b) 1.0 else 0.0
+  def delta(a: Int, b: Int): Int =
+    if(a == b) 1 else 0
 
-  def cosineSimilarity(u: NDArray, v: NDArray): Float = {
-    var sum: Float = (u * v).sum
-    var usum: Float = (u * u).sum
-    var vsum: Float = (v * v).sum
-    sum / Math.sqrt(usum * vsum)
+  def cosineSimilarity(u: NDArray, v: NDArray): NDArray = {
+    // sum: NDArray, usum: NDArray, vsum: NDArray
+    NDArray.sum(u * v) / NDArray.sqrt(NDArray.sum(u * u) * NDArray.sum(v * v))
   }
 
   def makeTensor2(n: Int , m: Int): NDArray = NDArray.zeros(n, m)
@@ -28,20 +26,19 @@ object math {
 
   // Gemv(t Trans, alpha f64, A General, x Vec, beta f64, y Vec)
   // y = alpha * A * x + beta * y; if t == blas.NoTrans
-  def Gemv(trans: Boolean, alpha: Float, M2: Array[Array[Float]],
-    x: Array[Float], beta: Float, y: Array[Float]): Unit = {
+  def Gemv(trans: Boolean, alpha: Float, M2: NDArray,
+    x: NDArray, beta: Float, y: NDArray): NDArray = {
     // y = alpha * A * x + beta * y; if t == blas.NoTrans
-    val n = x.size
-    val m = y.size
-    for(i <- 0 until n; j <- 0 until m)
-      y(j) = alpha * (if(trans) M2(i)(j) else M2(j)(i)) * x(i) + beta * y(j)
+    if(trans) NDArray.dot(NDArray.transpose(M2), x) * alpha + y * beta
+    else NDArray.dot(M2, x) * alpha + y * beta
+    // for(i <- 0 until n; j <- 0 until m)
+    //   y(j) = alpha * (if(trans) M2(i)(j) else M2(j)(i)) * x(i) + beta * y(j)
   }
 
-  def Ger(alpha: Double, x: Array[Double], y: Array[Double], A: Array[Array[Double]]): Unit = {
+  def Ger(alpha: Float, x: NDArray, y: NDArray, A: NDArray): NDArray = {
     // A += alpha * x * y^T
-    val n = x.size
-    val m = y.size
-    for(i <- 0 until m; j <- 0 until n)
-      A(j)(i) += alpha * x(j) * y(i)
+    A + x * NDArray.transpose(y) * alpha
+    // for(i <- 0 until m; j <- 0 until n)
+    //   A(j)(i) += alpha * x(j) * y(i)
   }
 }
--- src/test/scala/ntm/math_test.scala
+++ src/test/scala/ntm/math_test.scala
@@ -1,5 +1,7 @@
 package com.scalaml.ntm
 
+import org.apache.mxnet._
+
 import math._
 
 object math_test {
@@ -9,11 +11,11 @@ object math_test {
       else t.Errorf(s"[MATH] wrong math $s")
     }
 
-    checkEqual("Sigmoid(0)", Sigmoid(0) - 0.5)
-    checkEqual("Sigmoid(1)", Sigmoid(1) - 0.7310585786300049)
-    checkEqual("Sigmoid(-1)", Sigmoid(-1) - 0.2689414213699951)
-    checkEqual("Sigmoid(10)", Sigmoid(10) - 0.9999546021312976)
-    checkEqual("Sigmoid(-10)", Sigmoid(-10) - 4.5397868702434395E-5)
+    checkEqual("Sigmoid(0)", Sigmoid(NDArray.array(Array(0), Shape(1, 1))).toArray(0) - 0.5)
+    checkEqual("Sigmoid(1)", Sigmoid(NDArray.array(Array(1), Shape(1, 1))).toArray(0) - 0.7310585786300049)
+    checkEqual("Sigmoid(-1)", Sigmoid(NDArray.array(Array(-1), Shape(1, 1))).toArray(0) - 0.2689414213699951)
+    checkEqual("Sigmoid(10)", Sigmoid(NDArray.array(Array(10), Shape(1, 1))).toArray(0) - 0.9999546021312976)
+    checkEqual("Sigmoid(-10)", Sigmoid(NDArray.array(Array(-10), Shape(1, 1))).toArray(0) - 4.5397868702434395E-5)
 
     checkEqual("delta(0, 0)", delta(0, 0) - 1)
     checkEqual("delta(1, 1)", delta(1, 1) - 1)
@@ -21,32 +23,31 @@ object math_test {
     checkEqual("delta(10, 10)", delta(1, 0))
     checkEqual("delta(-10, -10)", delta(-10, 10))
 
-    checkEqual("cosineSimilarity(Array(1, 0), Array(1, 0))", cosineSimilarity(Array(1, 0), Array(1, 0)) - 1.0)
-    checkEqual("cosineSimilarity(Array(1, 0), Array(2, 0))", cosineSimilarity(Array(1, 0), Array(2, 0)) - 1.0)
-    checkEqual("cosineSimilarity(Array(1, 2), Array(2, 3))", cosineSimilarity(Array(1, 2), Array(2, 3)) - 0.9922778767136677)
-    checkEqual("cosineSimilarity(Array(1, 2, 3), Array(2, 3, 4))", cosineSimilarity(Array(1, 2, 3), Array(2, 3, 4)) - 0.9925833339709302)
-    checkEqual("cosineSimilarity(Array(1, 2), Array(-2, -1))", cosineSimilarity(Array(1, 2), Array(-2, -1)) - -0.8)
+    checkEqual("cosineSimilarity(Array(1, 0), Array(1, 0))", cosineSimilarity(NDArray.array(Array(1, 0), Shape(2, 1)), NDArray.array(Array(1, 0), Shape(2, 1))).toArray(0) - 1.0)
+    checkEqual("cosineSimilarity(Array(1, 0), Array(2, 0))", cosineSimilarity(NDArray.array(Array(1, 0), Shape(2, 1)), NDArray.array(Array(2, 0), Shape(2, 1))).toArray(0) - 1.0)
+    checkEqual("cosineSimilarity(Array(1, 2), Array(2, 3))", cosineSimilarity(NDArray.array(Array(1, 2), Shape(2, 1)), NDArray.array(Array(2, 3), Shape(2, 1))).toArray(0) - 0.9922778767136677)
+    checkEqual("cosineSimilarity(Array(1, 2, 3), Array(2, 3, 4))", cosineSimilarity(NDArray.array(Array(1, 2, 3), Shape(3, 1)), NDArray.array(Array(2, 3, 4), Shape(3, 1))).toArray(0) - 0.9925833339709302)
+    checkEqual("cosineSimilarity(Array(1, 2), Array(-2, -1))", cosineSimilarity(NDArray.array(Array(1, 2), Shape(2, 1)), NDArray.array(Array(-2, -1), Shape(2, 1))).toArray(0) - -0.8)
 
     checkEqual("makeTensor2(2, 3).size", makeTensor2(2, 3).size - 2)
     checkEqual("makeTensor2(2, 3).head.size", makeTensor2(2, 3).head.size - 3)
 
-    var y = Array[Double]()
-    y = Array(7.0, 8.0)
-    Gemv(false, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y)
+    var y = Array[Float]()
+    y = Gemv(false, 0.5, NDArray.array(Array(1, 2, 3, 4), Shape(2, 2)),
+      NDArray.array(Array(5, 6), Shape(2, 1)), NDArray.array(Array(7, 8), Shape(2, 1))).toArray
     checkEqual("Gemv(false, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y) y(0)", y(0) - 6.1425)
     checkEqual("Gemv(false, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y) y(1)", y(1) - 12.395)
 
-    y = Array(7.0, 8.0)
-    Gemv(true, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y)
+    y = Gemv(true, 0.5, NDArray.array(Array(1, 2, 3, 4), Shape(2, 2)),
+      NDArray.array(Array(5, 6), Shape(2, 1)), NDArray.array(Array(7, 8), Shape(2, 1))).toArray
     checkEqual("Gemv(true, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y) y(0)", y(0) - 9.1425)
     checkEqual("Gemv(true, 0.5, Array(Array(1, 2), Array(3, 4)), Array(5, 6), 0.05, y) y(1)", y(1) - 12.27)
 
-    var a = Array[Array[Double]]()
-    a = Array(Array(1, 2), Array(3, 4))
-    Ger(0.5, Array(5, 6), Array(7, 8), a)
-    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(0)(0)", a(0)(0) - 18.5)
-    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(0)(1)", a(0)(1) - 22.0)
-    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(1)(0)", a(1)(0) - 24.0)
-    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(1)(1)", a(1)(1) - 28.0)
+    y = Ger(0.5, NDArray.array(Array(5, 6), Shape(2, 1)), NDArray.array(Array(7, 8), Shape(2, 1)),
+      NDArray.array(Array(1, 2, 3, 4), Shape(2, 2))).toArray
+    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(0)(0)", a(0) - 18.5)
+    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(0)(1)", a(1) - 22.0)
+    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(1)(0)", a(2) - 24.0)
+    checkEqual("Ger(0.5, Array(5, 6), Array(7, 8), a) a(1)(1)", a(3) - 28.0)
   }
 }
