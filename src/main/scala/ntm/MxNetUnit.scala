2b4efbaa5dd026854c481b95034cc2d0a35c0b96
Wei Chen <weichentaiwan@gmail.com>
2019-06-30 16:35:02 +0800

    mxnet-unit

--- src/main/scala/ntm/MxNetUnit.scala
+++ src/main/scala/ntm/MxNetUnit.scala
@@ -9,23 +9,18 @@ class MxNetUnit (
   var Val: NDArray = NDArray.zeros(1), // value at node
   var Grad: NDArray = NDArray.zeros(1) // gradient at node
 ){
-  def String(): String = s"{$Val $Grad}"
+  def String(): String = s"{${Val.toArray.mkString(",")} ${Grad.toArray.mkString(",")}}"
 }
 
 object MxNetUnit {
 
-  def makeTensorUnit2(n: Int, m: Int): Array[Array[MxNetUnit]] =
-    Array.fill[MxNetUnit](n, m)(new MxNetUnit)
+  def makeMxNetUnit1(n: Int): MxNetUnit =
+    new MxNetUnit(NDArray.zeros(n, 1), NDArray.zeros(n, 1))
 
-  def makeTensorUnit3(n: Int, m: Int, p: Int): Array[Array[Array[MxNetUnit]]] =
-    Array.fill[MxNetUnit](n, m, p)(new MxNetUnit)
+  def makeMxNetUnit2(n: Int, m: Int): MxNetUnit =
+    new MxNetUnit(NDArray.zeros(n, m), NDArray.zeros(n, m))
 
-  def doUnit1(t: Array[MxNetUnit], f: MxNetUnit => Unit): Unit =
-    t.foreach(u => f(u))
-
-  def doUnit2(t: Array[Array[MxNetUnit]], f: MxNetUnit => Unit): Unit =
-    t.foreach(a => doUnit1(a, f))
-
-  def doUnit3(t: Array[Array[Array[MxNetUnit]]], f: MxNetUnit => Unit): Unit =
-    t.foreach(a => doUnit2(a, f))
+  def makeMxNetUnit3(n: Int, m: Int, p: Int): MxNetUnit =
+    new MxNetUnit(NDArray.zeros(n, m, p), NDArray.zeros(n, m, p))
+  
 }
